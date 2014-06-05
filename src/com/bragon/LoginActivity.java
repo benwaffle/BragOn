d845c2d2d8a1952f01c3c576a1fb5dc93c5bfca9
Ben Iofel <iofelben@gmail.com>
2014-06-05 09:05:29 -0400

    created login view

--- src/com/bragon/LoginActivity.java
+++ src/com/bragon/LoginActivity.java
@@ -2,14 +2,25 @@ package com.bragon;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-    }
+	Activity me = this;
+	private Button psLogin, fbLogin;
+	private EditText psU, psP, fbU, fbP;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+
+		psLogin = (Button)findViewById(R.id.psLogin);
+		fbLogin = (Button)findViewById(R.id.fbLogin);
+		psU = (EditText)findViewById(R.id.psUsername);
+		psP = (EditText)findViewById(R.id.psPassword);
+		fbU = (EditText)findViewById(R.id.fbEmail);
+		fbP = (EditText)findViewById(R.id.fbPassword);	
+	}
 }
--- src/com/bragon/MainActivity.java
+++ src/com/bragon/MainActivity.java
@@ -1,9 +1,8 @@
 package com.bragon;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +10,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		startActivity(new Intent(this, LoginActivity.class));
 	}
 }
